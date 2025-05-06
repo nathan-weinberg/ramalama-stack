380b5eb56c447d22a14ac7cc45720bafa23b8d3e
Nathan Weinberg <31703736+nathan-weinberg@users.noreply.github.com>
2025-05-06 16:13:42 -0400

    ci: add job to test ramalama llama-stack container (#63)
    Signed-off-by: Nathan Weinberg <nweinber@redhat.com>

--- tests/test-container.sh
+++ tests/test-container.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 main() {
-  echo "===> starting 'test-external-providers'..."
+  echo "===> starting 'test-container'..."
   start_and_wait_for_ramalama_server
   test_ramalama_chat_completion
-  start_and_wait_for_llama_stack_server
+  start_and_wait_for_llama_stack_container
   test_llama_stack_chat_completion
   test_llama_stack_openai_chat_completion
-  echo "===> 'test-external-providers' completed successfully!"
+  echo "===> 'test-container' completed successfully!"
 }
 
 TEST_UTILS=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
